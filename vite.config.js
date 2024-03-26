0a2c4dd45782c81d6ae8ee659600c0dda8b18104
Konrad <kondykox@gmail.com>
2024-03-26 15:49:47 +0100

    vite config changes

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
-import replace from "@rollup/plugin-replace";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    replace({
-      "process.env.VITE_API_KEY": JSON.stringify(process.env.VITE_API_KEY),
-    }),
-  ],
+  plugins: [react()],
 });
